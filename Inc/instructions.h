26bfe65a9e20498c6076660cd8b28b6fb71bbaae
zhiyb <yz39g13@soton.ac.uk>
2014-12-25 18:22:18 +0000

    Source code initial

---
+++ Inc/instructions.h
@@ -0,0 +1,19 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+//#define PKG_SIZE	32
+//#define PKG_SYNC	(uint16_t)0xAA55
+
+#define CMD_NOP		'\x00'
+#define CMD_RESET	'R'
+#define CMD_PAUSE	'P'
+#define CMD_CONF	'C'
+#define CMD_INFO	'I'
+#define CMD_ACK		'A'
+#define CMD_END		'E'
+
+#define CMD_ADC		'\x01'
+
+#define CMD_DAC		'\x02'
+
+#endif
