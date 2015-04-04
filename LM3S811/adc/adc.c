cd4a75ec048bfaee901479b718137cc004629a6b
Norman Zhi <yz39g13@soton.ac.uk>
2015-04-04 18:10:34 +0100

    Fix for ADC channel disordered at high sample frequency
    Timer cannot be stopped at high sample frequency (chip bug?)
    Reduced max ADC sample frequency to 400kSps

--- LM3S811/adc/adc.c
+++ LM3S811/adc/adc.c
@@ -100,11 +100,11 @@ long ADCSeqDataGet(unsigned long ulBase, unsigned long ulSequenceNum,
     return(ulCount);
 }
 
-long ADCSeqDataGetNum(unsigned long ulBase, unsigned long ulSequenceNum, unsigned long ulNum, adcData_t *pulBuffer)
+unsigned long ADCSeqDataGetNum(unsigned long ulBase, unsigned long ulSequenceNum, unsigned long ulNum, adcData_t *pulBuffer)
 {
 	unsigned long ulCount = 0;
 	ulBase += ADC_SEQ + (ADC_SEQ_STEP * ulSequenceNum);
-	while (!(HWREG(ulBase + ADC_SSFSTAT) & ADC_SSFSTAT0_EMPTY) && ulCount != ulNum) {
+	while (ulCount != ulNum && !(HWREG(ulBase + ADC_SSFSTAT) & ADC_SSFSTAT0_EMPTY)) {
 		*pulBuffer++ = HWREG(ulBase + ADC_SSFIFO);
 		ulCount++;
 	}
@@ -143,7 +143,6 @@ static inline void pauseADC(void)
 
 static inline void resumeADC(void)
 {
-	ADCIntClear(ADC, 0);
 	startTimer0();
 }
 
@@ -170,10 +169,8 @@ static void startADC(void)
 		adcBufferCurrent = adcBufferStart = (adcData_t *)(adcTxBuffer + ADC_PREPEND_BYTES);
 		adcBufferEnd = adcBufferStart + adcBufferCount;
 	}
-
-	// Device specific
-	//ADCSequenceEnable(ADC, 0);
-
+	adcData_t adc[8];
+	while (ADCSeqDataGet(ADC, 0, adc));
 	resumeADC();
 }
 
@@ -183,10 +180,6 @@ static inline void stopADC(void)
 	// FIXME: Proper delay required here
 	volatile unsigned long i = SYS_CLK / 1000;
 	while (i--);
-	adcData_t adc[16];
-	while (ADCSeqDataGet(ADC, 0, adc));
-	//ADCSequenceDisable(ADC, 0);
-	ADCIntClear(ADC, 0);
 	adcTxBufferRequest = 0;
 }
 
@@ -264,16 +257,11 @@ void ctrlADCControllerGenerate(void)
 
 void ADCIntHandler(void)
 {
-	unsigned long cnt;
-	cnt = ADCSeqDataGetNum(ADC, 0, channelCount, adcBufferCurrent);
-	//cnt = ADCSeqDataGet(ADC0_BASE, 0, adcBufferCurrent);
 	ADCIntClear(ADC, 0);
-
-	// Incorrect data length
-	if (cnt != channelCount)
+	if (ADCSeqDataGetNum(ADC, 0, channelCount, adcBufferCurrent) != channelCount)
 		return;
 
-	adcBufferCurrent = adcBufferCurrent + cnt == adcBufferEnd ? adcBufferStart : adcBufferCurrent + cnt;
+	adcBufferCurrent = adcBufferCurrent + channelCount == adcBufferEnd ? adcBufferStart : adcBufferCurrent + channelCount;
 
 	if (scanMode) {
 		if (pause)
@@ -290,245 +278,6 @@ void ADCIntHandler(void)
 
 	pauseADC();
 	sendData(adcTxBuffer, adcTxBufferLength);
-	poolSending();
+	adcBufferCurrent = adcBufferStart;
 	resumeADC();
 }
-
-
-
-
-
-// Original ******************************************
-#if 0
-static uint32_t channelEnabled = 0xFFFFFFFF, channelCount = LIM_ADC_CHANNELS;
-
-static struct buffer_t {
-	uint8_t enabled;
-	unsigned long current, length;
-	uint16_t buffer[LIM_BUFFER_SIZE / 2];
-} buffer;
-
-static struct trigger_t {
-	enum TriggerType {RisingEdge = 0, FallingEdge = 1} type;
-	enum TriggerStatuses {New, Before, Waiting, After} status;
-	uint8_t enabled, channel;
-	uint16_t level, previousData;
-	unsigned long preLength, postLength, count;
-} trigger;
-
-// *************************** ADC ***************************
-
-void adcSequenceUpdate(void);
-void timerDisable(void);
-void timerEnable(void);
-void timerSet(const unsigned long load);
-
-void adcLimitsPut(void)
-{
-	UARTCharPut(UART0_BASE, LIM_ADC_CHANNELS);
-	UARTCharPut(UART0_BASE, LIM_ADC_BITS);
-	UARTLongPut(UART0_BASE, LIM_ADC_MAXSPEED);
-	UARTLongPut(UART0_BASE, LIM_ADC_RTSPEED);
-	UARTCharPut(UART0_BASE, LIM_ADC_REFERENCE_I);
-	UARTCharPut(UART0_BASE, LIM_ADC_REFERENCE_F);
-}
-
-void adcConfigure(void)
-{
-	switch (UARTCharGet(UART0_BASE)) {
-	case COM_LIMITS:
-		adcLimitsPut();
-		UARTCharPut(UART0_BASE, COM_ACK);
-		break;
-	case COM_SET:
-		channelEnabled = UARTLongGet(UART0_BASE);
-		buffer.enabled = UARTCharGet(UART0_BASE);
-		adcSequenceUpdate();
-		UARTCharPut(UART0_BASE, COM_ACK);
-		break;
-	case COM_START:
-		UARTCharPut(UART0_BASE, COM_ACK);
-		ADCIntClear(ADC0_BASE, 0);
-		ADCSequenceEnable(ADC0_BASE, 0);
-		//ADCIntEnable(ADC0_BASE, 0);
-		break;
-	case COM_STOP:
-		//ADCIntDisable(ADC0_BASE, 0);
-		ADCSequenceDisable(ADC0_BASE, 0);
-		UARTCharPut(UART0_BASE, COM_ACK);
-		break;
-	}
-}
-
-static unsigned long resolveSequence(const unsigned long channel)
-{
-	unsigned long mask = 0x00000001, ch = 0, seq = 0;
-	for (ch = 0; ch != channel && ch != LIM_ADC_CHANNELS; ch++) {
-		if (channelEnabled & mask)
-			seq++;
-		mask <<= 1;
-	}
-	return seq;
-}
-
-// *************************** Trigger ***************************
-
-static inline enum TriggerStatuses triggerHandle(const unsigned long data)
-{
-	switch (trigger.status) {
-	case New:
-		trigger.status = trigger.preLength <= 1 ? Waiting : Before;
-		trigger.count = 1;
-		break;
-	case Before:
-		if (++trigger.count >= trigger.preLength) {
-			trigger.status = Waiting;
-			trigger.count = 1;
-		}
-		break;
-	case Waiting:
-		if ((trigger.type == RisingEdge && trigger.previousData <= trigger.level && data >= trigger.level && trigger.previousData < data) || \
-		    (trigger.type == FallingEdge && trigger.previousData >= trigger.level && data <= trigger.level && trigger.previousData > data))
-			trigger.status = trigger.postLength <= 1 ? New : After;
-		break;
-	case After:
-		if (++trigger.count >= trigger.postLength)
-			trigger.status = New;
-	}
-	trigger.previousData = data;
-	return trigger.status;
-}
-
-void adcTriggerInit(void)
-{
-	adcTriggerReset();
-}
-
-void adcTriggerReset(void)
-{
-	trigger.status = New;
-}
-
-void adcTriggerConfigure(void)
-{
-	switch (UARTCharGet(UART0_BASE)) {
-	case COM_SET:
-		trigger.channel = UARTCharGet(UART0_BASE);
-		trigger.type = UARTCharGet(UART0_BASE);
-		trigger.level = UARTLongGet(UART0_BASE);
-		trigger.preLength = UARTLongGet(UART0_BASE);
-		trigger.postLength = UARTLongGet(UART0_BASE);
-		trigger.channel = resolveSequence(trigger.channel);
-		UARTCharPut(UART0_BASE, COM_ACK);
-		break;
-	case COM_START:
-		UARTCharPut(UART0_BASE, COM_ACK);
-		trigger.enabled = 1;
-		break;
-	case COM_STOP:
-		trigger.enabled = 0;
-		UARTCharPut(UART0_BASE, COM_ACK);
-		break;
-	}
-}
-
-// *************************** Buffer ***************************
-
-void adcBufferInit(void)
-{
-	buffer.enabled = 0;
-	buffer.length = 0;
-	adcBufferReset();
-	//bufferInit();
-}
-
-void adcBufferReset(void)
-{
-	buffer.current = 0;
-}
-
-void adcBufferConfigure(void)
-{
-	switch (UARTCharGet(UART0_BASE)) {
-	case COM_LIMITS:
-		UARTLongPut(UART0_BASE, LIM_BUFFER_SIZE);
-		UARTCharPut(UART0_BASE, COM_ACK);
-		break;
-	case COM_SET:
-		adcBufferReset();
-		//bufferSetLength(UARTLongGet(UART0_BASE));
-		buffer.length = UARTLongGet(UART0_BASE) / 2;
-		UARTCharPut(UART0_BASE, COM_ACK);
-		break;
-	}
-}
-
-// *************************** Interrupt ***************************
-
-/*#define	ADC_SEQ			(ADC_O_SSMUX0)
-#define	ADC_SEQ_STEP		(ADC_O_SSMUX1 -	ADC_O_SSMUX0)
-#define	ADC_SSMUX		(ADC_O_SSMUX0 -	ADC_O_SSMUX0)
-#define	ADC_SSEMUX		(ADC_O_SSEMUX0 - ADC_O_SSMUX0)
-#define	ADC_SSCTL		(ADC_O_SSCTL0 -	ADC_O_SSMUX0)
-#define	ADC_SSFIFO		(ADC_O_SSFIFO0 - ADC_O_SSMUX0)
-#define	ADC_SSFSTAT		(ADC_O_SSFSTAT0	- ADC_O_SSMUX0)
-#define	ADC_SSOP		(ADC_O_SSOP0 - ADC_O_SSMUX0)
-#define	ADC_SSDC		(ADC_O_SSDC0 - ADC_O_SSMUX0)
-long ADCSequenceDataGetNum(unsigned long ulBase, unsigned long ulSequenceNum, unsigned long ulNum, unsigned long *pulBuffer)
-{
-	unsigned long ulCount = 0;
-	ulBase += ADC_SEQ + (ADC_SEQ_STEP * ulSequenceNum);
-	while (!(HWREG(ulBase + ADC_SSFSTAT) & ADC_SSFSTAT0_EMPTY) && (ulCount < 8)) {
-		*pulBuffer++ = HWREG(ulBase + ADC_SSFIFO);
-		ulCount++;
-	}
-	return ulCount;
-}*/
-
-void ADCIntHandler(void)
-{
-	unsigned long adc[8], cnt, i;
-	//cnt = ADCSequenceDataGetNum(ADC0_BASE, 0, channelCount, adc);
-	cnt = ADCSequenceDataGet(ADC0_BASE, 0, adc);
-	ADCIntClear(ADC0_BASE, 0);
-
-//	if (cnt != channelCount)
-//		return;
-
-	if (!buffer.enabled) {
-		UARTCharPut(UART0_BASE, COM_ADC_DATA);
-		for (i = 0; i != cnt; i++)
-			UARTShortPut(UART0_BASE, adc[i]);
-		return;
-	}
-
-	for (i = 0; i != cnt; i++) {
-		buffer.buffer[buffer.current] = adc[i];
-		if (++buffer.current == buffer.length)
-			buffer.current = 0;
-	}
-
-	if (trigger.enabled) {
-		if (triggerHandle(adc[trigger.channel]) != New)
-			return;
-	} else if (buffer.current != 0)
-			return;
-
-	// Output
-	ADCSequenceDisable(ADC0_BASE, 0);
-	timerDisable();
-	UARTCharPut(UART0_BASE, COM_ADC_FRAME);
-	for (i = 0; i != buffer.length; i++) {
-		//UARTShortPut(UART0_BASE, *(short *)bufferRead(sizeof(short)));
-		UARTShortPut(UART0_BASE, buffer.buffer[buffer.current]);
-		if (++buffer.current == buffer.length)
-			buffer.current = 0;
-	//} while (bufferPosition() != 0);
-	}
-	buffer.current = 0;
-	ADCSequenceDataGet(ADC0_BASE, 0, adc);
-	ADCIntClear(ADC0_BASE, 0);
-	ADCSequenceEnable(ADC0_BASE, 0);
-	timerEnable();
-}
-#endif
